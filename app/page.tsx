96c8f52952d70193a1ccc8686cdc92ed6283c6f8
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-11-25 00:47:33 +0000

    Added a button component from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
