177498ae36821efaf7fe25becbbfd2c3404600cf
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-12-05 02:16:32 +0000

    Component added - TeamList

---
+++ components/tournament/TeamList.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import TeamCard from "./TeamCard";
+
+interface TeamListProps {
+  teams: Array<{
+    name: string;
+    logo: string;
+    playerCount: number;
+  }>;
+  tournament: string;
+}
+
+const TeamList: React.FC<TeamListProps> = ({ teams, tournament }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {teams.map((team, index) => (
+        <TeamCard
+          key={index}
+          name={team.name}
+          logo={team.logo}
+          playerCount={team.playerCount}
+          tournament={tournament}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TeamList;
